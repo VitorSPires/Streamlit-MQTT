69c9a492a0d5ae99a80bd2a11db359078118bbe9
VitorSPires <vitorsantopires@hotmail.com>
2021-11-21 12:43:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,19 @@ import mqtt
 import streamlit as st
 import style
 
-
 style.setStyle()
 
 st.title('Teste')
 with st.expander('expander 1', expanded=True):
     option = st.radio(
         "What's your favorite movie genre",
-        ('Comedy', 'Drama', 'Documentary'))
+        ('Cena 1', 'Cena 2', 'Cena 3'))
 
 if st.button('Botão'):
     mqtt.publish(mqtt.client, 'teste0')
 
 with st.expander('expander 2', expanded=True):
-    if option == 'Comedy':
+    if option == 'Cena 1':
         if st.button('Gerenciador de tarefas'):
             mqtt.publish('teste')
         if st.button('Botão windows'):
@@ -29,7 +28,7 @@ with st.expander('expander 2', expanded=True):
             mqtt.publish('teste5')
         else: mqtt.publish('teste6')
 
-    elif option == 'Drama':
+    elif option == 'Cena 2':
         st.text('nada aqui')
 
     else:
