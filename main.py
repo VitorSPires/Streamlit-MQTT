b2b113b4685f2d94b238316b8902688e44f3c7f7
VitorSPires <vitorsantopires@hotmail.com>
2021-11-27 17:51:08 -0300

    att

--- main.py
+++ main.py
@@ -5,45 +5,44 @@ import style
 style.setStyle()
 client = mqtt.setup()
 
+cenas = ['Pré-culto', 'Cronômetro', 'Culto', 'Pós-culto', 'Problemas', 'Temporário']
+canalTeste = "vitor/teste/canal1"
+
 st.title('Controle OBS')
 with st.expander('Cenas', expanded=True):
     option = st.radio(
         'É necessário fazer transição após escolher uma cena',
-        ('Cena 1', 'Cena 2', 'Cena 3', 'Teste'))
+        cenas)
 
 if st.button('Transição', key='btnTransicao'):
-        client.publish("vitor/teste/canal1", 'Transição')
+        client.publish("vitor/teste/canal1", 'Transicao')
 
-canalTeste = "vitor/teste/canal1"
+if  option == cenas[0]:
+    client.publish(canalTeste, 'preCulto')
 
-with st.expander('Fontes', expanded=True):
-    if option == 'Cena 1':
-        client.publish(canalTeste, 'cena1')
-        if st.button('Fonte 1'):
-            client.publish(canalTeste, '')
-        if st.button('Fonte 2'):
-            client.publish(canalTeste, '')
-        if st.button('Fonte 3'):
-            client.publish(canalTeste, '')
-        if st.button('Fonte 4'):
-            client.publish(canalTeste, '')
+if  option == cenas[1]:
+    client.publish(canalTeste, 'Cronometro')
 
-    elif option == 'Cena 2':
-        client.publish(canalTeste, 'cena2')
-        st.text('nada aqui')
+elif option == cenas[2]:
+    client.publish(canalTeste, 'CenaCulto')
+    with st.expander('Fontes', expanded=True):
+        if st.button('Avisos'):
+            client.publish(canalTeste, 'Avisos')
+        if st.button('Like e sub'):
+            client.publish(canalTeste, 'LikeSub')
+        if st.button('QR fixo'):
+            client.publish(canalTeste, 'QRFixo')
+        if st.button('QR ofertas'):
+            client.publish(canalTeste, 'QROferta')
+        if st.button('Culto presencial'):
+            client.publish(canalTeste, 'CultoPresencial')
 
-    elif option == 'Cena 3':
-        client.publish(canalTeste, 'cena3')
-        st.text('nada aqui tbm')
+elif  option == cenas[3]:
+    client.publish(canalTeste, 'PosCulto')
 
-    if option == 'Teste':
-        client.publish(canalTeste, 'cenaTeste')
-        if st.button('Gerenciador de tarefas'):
-            client.publish(canalTeste, 'teste1')
-        if st.button('Botão windows'):
-            client.publish(canalTeste, 'teste2')
-        if st.button('esc'):
-            client.publish(canalTeste, 'teste3')
-        if st.button('alt f4'):
-            client.publish(canalTeste, 'teste4')
+elif  option == cenas[4]:
+    client.publish(canalTeste, 'Problemas')
+
+elif  option == cenas[5]:
+    client.publish(canalTeste, 'Temporario')
 
